1b262a13c53ad2bf439d3dd8f7b763a3aef34b9a
Julie Padilla <padilla410@gmail.com>
2021-11-16 16:28:19 -0500

    working pipeline corson-dosch rewrite

--- 2_process/src/clean_up_data.R
+++ 2_process/src/clean_up_data.R
@@ -1,5 +1,14 @@
-clean_up_data <- function(csv_file) {
-  readr::read_csv(csv_file, col_types = 'iccd') %>%
+# Load necessary packages
+library(dplyr)
+library(stringr)
+
+# Function to process data
+process_data <- function(output_path, input_data) {
+  # load data
+  prediction_data <- readr::read_csv(input_data, col_types = 'iccd')
+  
+  # Add columns specifying color and point sizes
+  eval_data <- prediction_data %>%
     filter(str_detect(exper_id, 'similar_[0-9]+')) %>%
     mutate(col = case_when(
       model_type == 'pb' ~ '#1b9e77',
@@ -10,4 +19,8 @@ clean_up_data <- function(csv_file) {
       model_type == 'dl' ~ 22,
       model_type == 'pgdl' ~ 23
     ), n_prof = as.numeric(str_extract(exper_id, '[0-9]+')))
+  # write processed data to file
+  readr::write_csv(eval_data, path = output_path)
+  # return processed data
+  return(eval_data)
 }
